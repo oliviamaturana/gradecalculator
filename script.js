ecf75d5472a3b9ecea2067f53e1be8e51400c7e1
Olivia <oliviamaturanamcca@students.berkeley.net>
2018-12-08 15:21:04 -0800

    Project work yay

--- script.js
+++ script.js
@@ -1,69 +1,92 @@
-function calculateGrade(){
+var currentGrade;
 
-    var homeworkPoints = document.getElementById("homweworkPoints").value;
-    var homeworkWeight = document.getElementById("homeworkWeight").value;
+function calculateCurrentGrade(){
+    try {
+        var hwAvg = parseBoxAvg("homeworkPoints");
+        var homeworkWeight = parseBoxValue("homeworkWeight");
 
-    console.log(homeworkPoints);
-    console.log(homeworkWeight);
+        var cwAvg = parseBoxAvg("classworkPoints");
+        var classworkWeight = parseBoxValue("classworkWeight");
 
-    var classworkPoints = document.getElementById("classworkPoints").value;
-    var classworkWeight = document.getElementById("classworkWeight").value;
+        var assAvg = parseBoxAvg("assessmentPoints");
+        var assessmentWeight = parseBoxValue("assessmentWeight");
 
-    console.log(classworkPoints);
-    console.log(classworkWeight);
 
-    var assessmentPoints = document.getElementById("assessmentPoints").value;
-    var assessmentWeight = document.getElementById("assessmentWeight").value;
+        var parAvg = parseBoxAvg("participationPoints");
+        var participationWeight = parseBoxValue("participationWeight");
 
-    console.log(assessmentPoints);
-    console.log(assessmentWeight);
 
-    var participationPoints = document.getElementById("participationPoints").value;
-    var participationWeight = document.getElementById("partcipationWeight").value;
+        var hwTotal = (hwAvg * (homeworkWeight / 100));
+        console.log(hwTotal);
 
-    console.log(participationPoints);
-    console.log(participationWeight);
+        var cwTotal = (cwAvg * (classworkWeight / 100));
+        console.log(cwTotal);
 
-    var hwAvg = average(newNumber(homeworkPoints));
-    console.log(hwAvg);
+        var assTotal = (assAvg * (assessmentWeight / 100));
+        console.log(assTotal);
 
-    var cwAvg = average(newNumber(classworkPoints));
-    console.log(cwAvg);
+        var parTotal = (parAvg * (participationWeight / 100));
+        console.log(parTotal);
 
-    var assAvg = average(newNumber(assessmentPoints));
-    console.log(assAvg);
+        currentGrade = (hwTotal + cwTotal + assTotal + parTotal);
+        console.log("Your current grade is " + currentGrade + ".");
 
-    var parAvg = average(newNumber(participationPoints));
-    console.log(parAvg);
-
-    parseInt(homeworkWeight);
-    parseInt(classworkWeight);
-    parseInt(assessmentWeight);
-    parseInt(participationWeight);
-
-    var hwTotal = (hwAvg*(homeworkWeight/100));
-    console.log(hwTotal);
-
-    var cwTotal = (cwAvg*(classworkWeight/100));
-    console.log(cwTotal);
-
-    var assTotal = (assAvg*(assessmentWeight/100));
-    console.log(assTotal);
-
-    var parTotal = (parAvg*(partcipationWeight/100));
-    console.log(parTotal);
-
-    var currentGrade = (hwTotal + cwTotal + assTotal + parTotal);
-    console.log(currentGrade);
-
+        document.getElementById("currentGrade").innerHTML = "Your current grade is " + currentGrade + "%";
+    }
+    catch(error){
+        console.log("Error in current grade calculation " + error);
+        document.getElementById("currentGrade").innerHTML = "Error in current grade calculation " + error;
+    }
+    return currentGrade;
 
 }
 
-var newNumber(str){
+function calculateRequiredFinalGrade(){
+    try {
+
+        var desiredGrade = parseBoxValue("desiredGrade");
+
+        var finalWeight = parseBoxValue("finalWeight");
+
+        var requiredFinalGrade = (desiredGrade - currentGrade * (1 - finalWeight / 100)) / (finalWeight / 100);
+        console.log(requiredFinalGrade);
+
+        document.getElementById("requiredFinalGrade").innerHTML = "You need a " + requiredFinalGrade.toFixed(1) +
+            " to get a " + desiredGrade + " in the class.";
+    }
+    catch(error) {
+        console.log("Error in required grade calculation " + error);
+        document.getElementById("requiredFinalGrade").innerHTML = "Error in required final grade calculation " + error;
+    }
+}
+
+function parseBoxValue(boxId){
+    var valueString = document.getElementById(boxId).value;
+    var value = parseInt(valueString);
+    if (isNaN(value)){
+        throw new Error("The data in " + boxId + " is not an integer.");
+    }
+    console.log(boxId + " is " + value);
+    return value;
+}
+
+function parseBoxAvg(boxId){
+    var valueString = document.getElementById(boxId).value;
+    var values = newNumber(valueString, boxId);
+    var averageValue = average(values);
+    console.log(boxId + " average is " + averageValue);
+    return averageValue;
+}
+
+function newNumber(str, boxId){
     var arr = str.split(",");
     for(var i=0; i<arr.length; i++){
         arr[i] = parseInt(arr[i]);
+        if(isNaN(arr[i])){
+            throw new Error("The data in " + boxId + " is not a list of integers.");
+        }
     }
+
     console.log(arr);
     return arr;
 }
@@ -75,4 +98,3 @@ function average(arr){
     }
     return sum/(arr.length);
 }
-
